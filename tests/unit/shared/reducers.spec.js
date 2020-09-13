1d0ccac86ec8b1399b73eccddec0b712b87f063c
Steliana Budai <steliana.budai@acuris.com>
2020-09-13 16:07:26 +0300

    add test fro reset skip

--- tests/unit/shared/reducers.spec.js
+++ tests/unit/shared/reducers.spec.js
@@ -1,10 +1,14 @@
 import reducers from '../../../src/shared/reducers'
 import songs from '../../data/songs'
-import {FETCH_SONGS} from '../../../src/shared/reducers/actions'
+import {FETCH_SONGS, RESET_SKIP} from '../../../src/shared/reducers/actions'
 
 describe("Reducers ", () => {
     test("FETCH_SONGS concatenates songs to the state", () => {
       const newState =  reducers({}, {type: FETCH_SONGS, songs})  
       expect(newState.songs).toEqual(songs)
-    });
-  });
+    })
+    test("RESET_SKIP resets skipped items to 0", () => {
+        const newState =  reducers({skip:78}, {type: RESET_SKIP})  
+        expect(newState.skip).toEqual(0)
+      })
+  })
