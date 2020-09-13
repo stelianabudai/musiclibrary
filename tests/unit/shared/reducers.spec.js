b6e57fc658e4ec66849ace7fcccbe29926865f88
Steliana Budai <steliana.budai@acuris.com>
2020-09-13 15:49:36 +0300

    add skelet for tests

---
+++ tests/unit/shared/reducers.spec.js
@@ -0,0 +1,8 @@
+import reducers from '../../../src/shared/reducers'
+
+
+describe("Reducers ", () => {
+    test("FETCH_SONGS concatenate songs to the state", () => {
+        reducers({}, {})   
+      });
+  });
