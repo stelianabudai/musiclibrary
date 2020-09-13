1b7fa7044f0d3f0e340aa99d0d0193490da575fe
“stelianabudai” <sstelutzza@yahoo.com>
2020-09-13 10:00:30 +0300

    delete  not used file

--- src/server/middlawares/error.js
+++
@@ -1,8 +0,0 @@
-export default (err, req, res, next) => {
-    // formulate an error response here
-    console.log('sdsdsdsdsd');
-    if(err){
-        return res.status(500).send('An error ocurred. Please try later')
-    }
-    next()
-  }
--- src/server/routers/genreRouter.js
+++ src/server/routers/genreRouter.js
@@ -4,13 +4,12 @@ import Type from '../models/Genre'
 
 genreRouter.post('/', async(req, resp) => {
     try{
-    await Type.create({...req.body})
-    resp.sendStatus(200)
+         await Type.create({...req.body})
+         resp.sendStatus(200)
     }catch(error){
         //todo add logger 
         console.log(error)
         return res.status(500).send('An error has occured')  
-
     }
 })
 
