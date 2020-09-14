c2554b33a04c0c9cefb43021aec8ffeec873c16a
“stelianabudai” <sstelutzza@yahoo.com>
2020-09-14 19:05:11 +0300

    added new test with nock

---
+++ tests/unit/controllers/songsController.spec.js
@@ -0,0 +1,17 @@
+import {fetchSongs} from '../../../src/client/controllers/songsController'
+const dispatch = jest.fn()
+import songs from '../../data/songs.json'
+const nock = require('nock')
+
+describe("Song controller", () => {
+    test("fetchSongs", () => {
+        const limit = 5
+        const skip=3
+        const  genreId = '5f5a8b23c67a3f0ee1e6a7ab'
+        nock('/songs?limit=5&skip=3&&genreId=5f5a8b23c67a3f0ee1e6a7ab').get('/').reply(200, songs)
+
+        fetchSongs(limit, skip, genreId, dispatch)
+        expect(dispatch.calledOnce).toBe.true;   
+
+    })
+})
