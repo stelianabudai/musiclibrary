6d6182b5dc81a8b0814d7b509aa81f1ae9acfa0f
“stelianabudai” <sstelutzza@yahoo.com>
2020-09-14 19:14:16 +0300

    add test for fetchSongs when error

--- tests/unit/controllers/songsController.spec.js
+++ tests/unit/controllers/songsController.spec.js
@@ -1,10 +1,11 @@
 import {fetchSongs} from '../../../src/client/controllers/songsController'
 const dispatch = jest.fn()
+const sendError = jest.fn()
 import songs from '../../data/songs.json'
 const nock = require('nock')
 
 describe("Song controller", () => {
-    test("fetchSongs", () => {
+    test("fetchSongs positive scenario", () => {
         const limit = 5
         const skip=3
         const  genreId = '5f5a8b23c67a3f0ee1e6a7ab'
@@ -12,6 +13,16 @@ describe("Song controller", () => {
 
         fetchSongs(limit, skip, genreId, dispatch)
         expect(dispatch.calledOnce).toBe.true;   
+    })
 
+    test("fetchSongs when error", () => {
+        const limit = 5
+        const skip=3
+        const  genreId = '5f5a8b23c67a3f0ee1e6a7ab'
+        nock(`/songs?limit=${limit}&skip=${skip}&&genreId=${genreId}`).get('/').reply(500)
+
+        fetchSongs(limit, skip, genreId, dispatch, sendError)
+        expect(sendError.calledOnce).toBe.true;   
+        expect(dispatch.calledOnce).toBe.false;   
     })
 })
